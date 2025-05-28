147051ed29195e3c9656cc48616e73767b64a20f
Thomas O. Bender <145614085+CodingShade@users.noreply.github.com>
2025-05-27 21:42:01 -0300

    27/05

--- Tk/main.py
+++ Tk/main.py
@@ -1,23 +1,35 @@
 import tkinter as tk
 
-def imprimir_infos():  
-    global trocar_msg
-    trocar_msg = not trocar_msg
-    if trocar_msg:
-        rotulo.config(text="Olá, Mundo!")
-    else:
-        rotulo.config(text="Adeus, Mundo!")
+janela = tk.Tk()
+janela.title("Exemplo Radiobutton")
+janela.geometry("250x150")
+def mostrar_opcao(botao):
+    print(botao)
+    texto = f"Escolheu: {opcao1.get()}"
+    texto += f" {opcao2.get()}"
+    texto += f" {opcao3.get()}"
+    rotulo.config(text= texto)
 
-trocar_msg = False
+    op = [opcao1.get(), opcao2.get(), opcao3.get()]
+    if op.count(True) == 3:
+        if opcao1.get() and opcao2.get():
+            if botao == "SAUDE":
+                opcao1.set(False)
+            if botao == "TEMPO":
+                opcao3.set(False)
+            if botao == "DINHEIRO":
+                opcao2.set(False)
     
-janela = tk.Tk() 
-janela.title("Minha Janela")
-janela.geometry("600x400")
 
-rotulo = tk.Label(janela, text="Olá, Mundo!")
-rotulo.pack(padx=20, pady=20)
+opcao1 = tk.BooleanVar()
+opcao2 = tk.BooleanVar()
+opcao3 = tk.BooleanVar()
 
-botao = tk.Button(janela, text="Clique aqui", command=imprimir_infos)
-botao.pack(padx=20, pady=20)
+tk.Checkbutton(janela, text="DINHEIRO", variable=opcao1, command=lambda:mostrar_opcao("DINHEIRO")).pack()
+tk.Checkbutton(janela, text="TEMPO", variable=opcao2, command=lambda:mostrar_opcao("TEMPO")).pack()
+tk.Checkbutton(janela, text="SAUDE", variable=opcao3, command=lambda:mostrar_opcao("SAUDE")).pack()
 
+
+rotulo = tk.Label(janela, text="Escolheu: A")
+rotulo.pack(pady=10)
 janela.mainloop()
